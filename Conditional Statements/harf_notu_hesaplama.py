981fdba7f7c5c0cdbdefa3f83750e8a9907e0be7
rakscc <rakscc@outlook.com>
2021-10-09 17:09:08 +0300

    update

--- Conditional Statements/harf_notu_hesaplama.py
+++ Conditional Statements/harf_notu_hesaplama.py
@@ -14,21 +14,21 @@ toplam_not = hesap_vize1 + hesap_vize2 + hesap_final
 if ( toplam_not >= 90):
     print("Notunuzu {} 'dır. Harf notunuz ise AA 'dır".format(toplam_not))
 elif ( toplam_not >= 85):
-    print("Notunuz {} 'dır . Hatf notunuz ise BA 'dır".format(toplam_not))
+    print("Notunuz {} 'dır . Harf notunuz ise BA 'dır".format(toplam_not))
 elif ( toplam_not >= 80):
-    print("Notunuz {} 'dır . Hatf notunuz ise BB 'dır".format(toplam_not))
+    print("Notunuz {} 'dır . Harf notunuz ise BB 'dır".format(toplam_not))
 elif (toplam_not >= 75):
-    print("Notunuz {} 'dır . Hatf notunuz ise CB 'dır".format(toplam_not))
+    print("Notunuz {} 'dır . Harf notunuz ise CB 'dır".format(toplam_not))
 elif ( toplam_not >= 70):
-    print("Notunuz {} 'dır . Hatf notunuz ise CC 'dır".format(toplam_not))
+    print("Notunuz {} 'dır . Harf notunuz ise CC 'dır".format(toplam_not))
 elif (toplam_not >= 65):
-    print("Notunuz {} 'dır . Hatf notunuz ise DC 'dır".format(toplam_not))
+    print("Notunuz {} 'dır . Harf notunuz ise DC 'dır".format(toplam_not))
 elif (toplam_not >= 60):
-    print("Notunuz {} 'dır . Hatf notunuz ise DD 'dır".format(toplam_not))
+    print("Notunuz {} 'dır . Harf notunuz ise DD 'dır".format(toplam_not))
 elif (toplam_not >= 55):
-    print("Notunuz {} 'dır . Hatf notunuz ise FD 'dır".format(toplam_not))
+    print("Notunuz {} 'dır . Harf notunuz ise FD 'dır".format(toplam_not))
 elif ( toplam_not <= 55):
-    print("Notunuz {} 'dır . Hatf notunuz ise FF 'dır".format(toplam_not))
+    print("Notunuz {} 'dır . Harf notunuz ise FF 'dır".format(toplam_not))
 else:
     print(" Lutfen dogru deger giriniz! ")
     
